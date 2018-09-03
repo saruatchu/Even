01fa237e8776719cf5cfcf35714b13f71b973e62
saru <sarumathiayyappan@gmail.com>
2018-09-03 14:00:24 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,17 @@
+# Even
+import java.io.*;
+import java.util.*;
+class Even
+{
+public static void main(String args[])
+{
+int i;
+for(i=6;i<=9;i++)
+{
+if(i%2=0)
+{
+System.out.println(i);
+}
+}
+}
+}
